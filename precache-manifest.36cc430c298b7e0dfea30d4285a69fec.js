6cd478ede87b56ebe53d757a275885713b371ba4
YuvanShankar <107350619+yuvan1492@users.noreply.github.com>
2022-08-02 19:38:22 +0530

    Updates

---
+++ precache-manifest.36cc430c298b7e0dfea30d4285a69fec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "25751c90ed0bc94f805b",
+    "url": "/test2/static/css/main.44f2bc73.chunk.css"
+  },
+  {
+    "revision": "25751c90ed0bc94f805b",
+    "url": "/test2/static/js/main.25751c90.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/test2/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "5957ecdec827996a0807",
+    "url": "/test2/static/js/2.5957ecde.chunk.js"
+  },
+  {
+    "revision": "1ccdb880ba790781d7e3",
+    "url": "/test2/static/js/runtime~main.1ccdb880.js"
+  },
+  {
+    "revision": "4d752c876393ff64bad5438b6c3be6fb",
+    "url": "/test2/index.html"
+  }
+];
